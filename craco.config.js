a94ee68398d73c2b1793f3246584fc89052b7ffa
NeilYQ <360842060@qq.com>
2022-02-08 17:33:55 +0800

    0.0.1

--- craco.config.js
+++ craco.config.js
@@ -1,10 +1,14 @@
 
 const path = require("path");
+const WebpackBar = require("webpackbar")
 
 module.exports = {
     webpack: {
         alias: {
             '@': path.resolve("src")
-        }
+        },
+        plugins: [
+            new WebpackBar()
+        ]
     }
 };
